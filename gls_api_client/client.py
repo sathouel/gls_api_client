af2cb4c787cb2642a820c49659c1134501f6087b
steven athouel <sathouel@gmail.com>
2020-08-23 16:40:36 +0300

    auth method

--- gls_api_client/client.py
+++ gls_api_client/client.py
@@ -1,12 +1,19 @@
+import base64
+
 import requests as rq
 
 class Client:
     BASE_URL = "https://api.gls-group.eu/public/v1"
 
-    def __init__(self, username, password, sandbox=False):
+    def __init__(self, username, password, language='fr', sandbox=False):
+        if language not in ['en', 'fr', 'dk', 'de']:
+            raise ValueError("Invalid Language")
+
         self._username = username
+        self._language = language
         self._sandbox = sandbox
         
+        self._session = rq.Session()
         self._authenticate(username, password)
 
         self._resources = {
@@ -16,10 +23,14 @@ class Client:
         }
 
     def _authenticate(self, username, password):
+        auth = base64.b64encode('{}:{}'.format(username, password).encode()).decode()
         headers = {
-
+            "Accept-Language": self._language,
+            "Accept": "application/json",
+            "Accept-Encoding": "gzip,deflate",
+            "Content-Type": "application/json",
+            "Authorization": "Basic {}".format(auth)
         }
-        self._session = rq.Session()
         self._session.headers = headers
 
     @property
